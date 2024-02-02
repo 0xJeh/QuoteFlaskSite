f449845ce2d494f44c00eaa75409d4033e780ec3
Jehmess <39226383+0xJeh@users.noreply.github.com>
2024-02-02 23:48:21 +0000

    0xjeh

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-# GitHub Codespaces ♥️ Flask
+# Steps
 
-Welcome to your shiny new Codespace running Flask! We've got everything fired up and running for you to explore Flask.
+1) install pacakages
+2) add files
+3) link to csv 
+4) edit css 
 
-You've got a blank canvas to work on from a git perspective as well. There's a single initial commit with the what you're seeing right now - where you go from here is up to you!
-
-Everything you do here is contained within this one codespace. There is no repository on GitHub yet. If and when you’re ready you can click "Publish Branch" and we’ll create your repository and push up your project. If you were just exploring then and have no further need for this code then you can simply delete your codespace and it's gone forever.
-
-To run this application:
-
-```
-flask --debug run
-```
+compile
+send to azure
--- app.py
+++ app.py
@@ -1,7 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template
+import csv
+import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+
+def get_random_quote():
+    try:
+        with open('quotes.csv', 'r') as csv_file:
+            quotes = list(csv.reader(csv_file))
+            if not quotes:
+                raise ValueError("No quotes found in the CSV file.")
+            return random.choice(quotes)
+    except FileNotFoundError:
+        raise FileNotFoundError("CSV file 'quotes.csv' not found.")
+    except Exception as e:
+        raise Exception(f"An error occurred: {e}")
+
+@app.route('/', methods=['GET'])
+def random_quote():
+    author, quote = get_random_quote()
+    return render_template('index.html', author=author, quote=quote)
+
+if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
